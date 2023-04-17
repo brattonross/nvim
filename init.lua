349cc92ba5189b219bddc64175f3911e508d05fb
Ross Bratton <ross.bratton@healthhero.com>
2023-04-17 16:25:26 +0100

    add lazygit and undotree

--- init.lua
+++ init.lua
@@ -47,6 +47,35 @@ require("lazy").setup({
 		end,
 	},
 	{
+		"akinsho/toggleterm.nvim",
+		version = "*",
+		config = function()
+			require("toggleterm").setup()
+
+			vim.keymap.set("n", "<leader>tg", function()
+				local Terminal = require("toggleterm.terminal").Terminal
+				local lazygit = Terminal:new({
+					cmd = "lazygit",
+					hidden = true,
+					direction = "float",
+				})
+				return lazygit:toggle()
+			end, { desc = "Show [T]erminal with [G]it status" })
+		end,
+		event = "VeryLazy",
+		lazy = true,
+	},
+	{
+		"mbbill/undotree",
+		config = function()
+			vim.keymap.set("n", "<leader>u", function()
+				vim.cmd("UndotreeToggle")
+			end, { desc = "[u] Toggle undotree" })
+		end,
+		event = { "BufReadPost", "BufNewFile" },
+		lazy = true,
+	},
+	{
 		"folke/zen-mode.nvim",
 		config = function()
 			require("zen-mode").setup({
