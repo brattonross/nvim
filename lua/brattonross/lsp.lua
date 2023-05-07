79e6e2d5f035ed71b7c915de640e96e60bcb4b85
Ross Bratton <bratton.ross@gmail.com>
2023-05-07 19:03:35 +0100

    remove comments

--- lua/brattonross/lsp.lua
+++ lua/brattonross/lsp.lua
@@ -17,11 +17,9 @@ local on_attach = function(_, bufnr)
 	nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
 	nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
 
-	-- See `:help K` for why this keymap
 	nmap("K", vim.lsp.buf.hover, "Hover Documentation")
 	nmap("<C-s>", vim.lsp.buf.signature_help, "Signature Documentation")
 
-	-- Lesser used LSP functionality
 	nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 end
 
