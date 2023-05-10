3574f4103a916b8290092c22a579bb662b5131c8
Ross Bratton <bratton.ross@gmail.com>
2023-05-10 02:00:11 +0100

    add autocommands

--- lua/brattonross/autocommands.lua
+++ lua/brattonross/autocommands.lua
@@ -1,10 +1,30 @@
+local function augroup(name)
+	return vim.api.nvim_create_augroup("brattonross_" .. name, { clear = true })
+end
+
 -- Highlight on yank
--- See `:help vim.highlight.on_yank()`
-local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
 vim.api.nvim_create_autocmd("TextYankPost", {
 	callback = function()
 		vim.highlight.on_yank()
 	end,
-	group = highlight_group,
+	group = augroup("highlight_on_yank"),
 	pattern = "*",
+})
+
+-- Resize splits on VimResized
+vim.api.nvim_create_autocmd("VimResized", {
+	callback = function()
+		vim.cmd("tabdo wincmd =")
+	end,
+	group = augroup("resize_splits"),
+})
+
+-- Set wrap and spellcheck for certain filetypes
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "markdown", "gitcommit" },
+	callback = function()
+		vim.opt_local.wrap = true
+		vim.opt_local.spell = true
+	end,
+	group = augroup("filetype_settings"),
 })
