dab5df3d2013e3c94f1556706fd8c7f4f3e51cd5
Ross Bratton <bratton.ross@gmail.com>
2023-05-16 23:16:03 +0100

    sort opts

--- lua/brattonross/set.lua
+++ lua/brattonross/set.lua
@@ -1,61 +1,44 @@
-vim.opt.guicursor = ""
-
-vim.opt.number = true
-vim.opt.relativenumber = true
-
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-
-vim.opt.smartindent = true
-
-vim.opt.wrap = false
-
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-vim.opt.undofile = true
-
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
-
-vim.opt.termguicolors = true
-
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
-
-vim.opt.updatetime = 50
-
-vim.opt.colorcolumn = "80"
-
-vim.opt.completeopt = "menuone,noselect"
-
--- Sync clipboard between OS and Neovim
-vim.opt.clipboard = "unnamedplus"
-
-vim.g.splitright = true
-vim.g.splitbelow = true
-vim.g.netrw_alto = 1
-vim.g.netrw_altv = 1
-
-vim.opt.laststatus = 3
 vim.cmd("highlight WinSeparator guibg=None")
 
-vim.opt.winbar = "%#lualine_c_normal#%m %f"
-
+vim.g.netrw_alto = 1
+vim.g.netrw_altv = 1
 vim.g.netrw_banner = 0
 vim.g.netrw_bufsettings = "noma nomod nu nobl nowrap ro"
+vim.g.splitbelow = true
+vim.g.splitright = true
 
+vim.opt.backup = false
+vim.opt.clipboard = "unnamedplus"
+vim.opt.colorcolumn = "80"
+vim.opt.completeopt = "menuone,noselect"
+vim.opt.expandtab = true
+vim.opt.guicursor = ""
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+vim.opt.isfname:append("@-@")
+vim.opt.laststatus = 3
+vim.opt.list = true
 vim.opt.listchars:append({
 	eol = "↵",
 	space = "⋅",
 	tab = "» ",
 })
-vim.opt.list = true
-
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.scrolloff = 8
+vim.opt.shiftwidth = 4
 vim.opt.showmode = false
+vim.opt.signcolumn = "yes"
+vim.opt.smartindent = true
+vim.opt.softtabstop = 4
+vim.opt.swapfile = false
+vim.opt.tabstop = 4
+vim.opt.termguicolors = true
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+vim.opt.updatetime = 50
+vim.opt.winbar = "%#lualine_c_normal#%m %f"
+vim.opt.wrap = false
 
 if vim.fn.has("nvim-0.9.0") == 1 then
 	vim.opt.splitkeep = "screen"
